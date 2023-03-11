fa324c659d491ce154d89f77eabfdbf9618920d2
Prince Radebe <princeradebe@gmail.com>
2023-03-11 21:33:47 +0200

    Refine Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,26 +6,29 @@ import SmallSquares from './hero/SmallSquares'
 const Hero = () => {
     return (
         <>
-            <div>
-                <h1 className='text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-brand-light-blue from-brand-magenta '>
+            <div className='mt-12'>
+                <h1 className='text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-brand-light-blue from-brand-magenta '>
                     Hi, <span className='text-white'>I'm Prince</span>
                     <br />
                     Front End
                     <br />
                     Developer
                 </h1>
-                <h1 className='relative text-4xl font-bold text-white border-brand-light-blue border-[1px]'>
+                <h1 className='relative mt-2 pl-2 text-3xl font-bold text-white border-brand-light-blue border-[1px] w-10/12'>
                     & UI/UX Designer
                     <SmallSquares yPosition={'top'} xPosition={'right'} />
                     <SmallSquares yPosition={'top'} xPosition={'left'} />
                     <SmallSquares yPosition={'bottom'} xPosition={'right'} />
                     <SmallSquares yPosition={'bottom'} xPosition={'left'} />
-                    {/* <div className='absolute top-[-38px] right-[-38px]'>
+                    <div className='absolute top-[-38px] right-[-38px]'>
                         <ImPen color='#9FD4F3' />
-                    </div> */}
+                    </div>
                     <div class="absolute border-b-[0.5px] border-dashed border-brand-light-blue h-8 transform rotate-90"></div>
                 </h1>
             </div>
+            <h3 className='mt-4 text-white'>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic libero possimus aut pariatur necessitatibus eum. Dolorum, voluptatem? Quasi provident facere ad ab mollitia, dolores quam asperiores cum natus quas dolor.
+            </h3>
             <div className='flex text-2xl text-brand-light-blue gap-8 mt-6'>
                 <FaLinkedinIn />
                 <FaGithub />
