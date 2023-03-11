2c544146e4cbfbbf79c0c961c3b8b47a794f4e1f
Prince Radebe <princeradebe@gmail.com>
2023-03-11 16:41:50 +0200

    Add border bottom for skills section

--- src/components/skills/SkillsComp.jsx
+++ src/components/skills/SkillsComp.jsx
@@ -4,7 +4,8 @@ const SkillsComp = ({ skill, years }) => {
     return (
         <div className='flex justify-between items-baseline'>
             <h3 className='text-2xl text-brand-light-blue font-semibold'>{skill}</h3>
-            <p className='text-white'>{years}</p>
+            <div className='grow border-b-[0.5px] border-dashed border-brand-light-blue opacity-50'></div>
+            <p className='text-brand-light-blue font-light'>{years}</p>
         </div>
     )
 }
