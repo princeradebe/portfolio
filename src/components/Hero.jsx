03aeb5113786881a6f25902dd3b13d72ec9cae2c
Prince Radebe <princeradebe@gmail.com>
2023-03-11 23:13:43 +0200

    Add screen size style on Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import SmallSquares from './hero/SmallSquares'
 const Hero = () => {
     return (
         <>
-            <div className='mt-24'>
+            <div className=' mt-24'>
                 <h1 className='text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-brand-light-blue from-brand-magenta '>
                     Hi, <span className='text-white'>I'm Prince</span>
                     <br />
@@ -23,10 +23,12 @@ const Hero = () => {
                     <div className='absolute top-[-34px] right-[-34px]'>
                         <ImPen color='#9FD4F3' />
                     </div>
-                    <div class="absolute border-b-[0.5px] border-dashed border-brand-light-blue h-8 transform rotate-90"></div>
+                    <div className="absolute border-r-[0.5px] top-[-400px] right-[50%] border-dashed border-brand-light-blue h-[400px] opacity-50"></div>
+                    <div className="absolute border-b-[0.5px] top-[50%] left-[-40px] border-dashed border-brand-light-blue w-[40px] opacity-50"></div>
+                    <p className='absolute right-[36%] bottom-[-30px] font-normal text-sm px-2 rounded-sm bg-brand-light-blue text-brand-blue'>1080 x 740</p>
                 </h1>
             </div>
-            <h3 className='mt-4 text-white'>
+            <h3 className='mt-12 text-white'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic libero possimus aut pariatur necessitatibus eum. Dolorum, voluptatem? Quasi provident facere ad ab mollitia, dolores quam asperiores cum natus quas dolor.
             </h3>
             <div className='flex text-2xl text-brand-light-blue gap-8 mt-6'>
