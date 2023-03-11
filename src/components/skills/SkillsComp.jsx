b178cb0fc056300bf0a1a213c240f32cbeed1bd3
Prince Radebe <princeradebe@gmail.com>
2023-03-12 01:51:55 +0200

    Font sizes for Skills section

--- src/components/skills/SkillsComp.jsx
+++ src/components/skills/SkillsComp.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const SkillsComp = ({ skill, years }) => {
     return (
         <div className='flex px-4 justify-between items-baseline'>
-            <h3 className='text-2xl text-brand-light-blue font-semibold'>{skill}</h3>
-            <div className='grow border-b-[0.5px] border-dashed border-brand-light-blue opacity-50'></div>
-            <p className='text-brand-light-blue font-light'>{years}</p>
+            <h3 className='text-2xl md:text-6xl text-brand-light-blue font-semibold'>{skill}</h3>
+            <div className='grow border-b-[0.5px] md:border-b-2 border-dashed border-brand-light-blue opacity-50'></div>
+            <p className='text-brand-light-blue font-light md:text-5xl'>{years}</p>
         </div>
     )
 }
