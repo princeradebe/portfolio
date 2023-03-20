5e62921b3d03e5de1b1db28df1431dac7af8ad96
Prince Radebe <princeradebe@gmail.com>
2023-03-20 19:10:39 +0200

    Add resume link to Button on Navbar

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Resume from '../../assets/Prince_Radebe_CV_2023.pdf'
 
 const Button = () => {
+    const downloadFile = () => {
+    }
+
     return (
-        <button className='text-brand-blue py-2 px-4 bg-brand-light-blue rounded-sm'>Download CV </button>
+        <a href={Resume} download={'Prince_Radebe_CV_2023.pdf'}>
+            <button className='text-brand-blue py-2 px-4 bg-brand-light-blue rounded-sm'>Download CV </button>
+        </a>
     )
 }
 
